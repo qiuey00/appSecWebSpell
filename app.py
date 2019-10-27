852e54e2bbd5217b6a027b838e4bf7b335ada46e
Jeff <qiuey00@gmail.com>
2019-10-26 22:13:03 -0400

    registration

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from config import Config
+# from config import Config
 from flask import Flask, render_template, flash, request
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, PasswordField
 import subprocess
 import os
 
 
 loginInfo = dict()
 
+class registerForm(Form):
+	uname = StringField(label='User Name', id='uname', validators=[validators.required()])
+	pword = PasswordField(label='Password', id='pword', validators=[validators.required()])
+	fa2 = StringField(label='2 Factor', id='2fa')
+	submit = SubmitField('Submit')
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('register.html')
 
-@app.route('register', methods=['GET','POST'])
-def register():
+# @app.route('/register', methods=['GET','POST'])
+# def register():
 	
 
-
 if __name__ == '__main__':
-    app.run()
+	app.run()
