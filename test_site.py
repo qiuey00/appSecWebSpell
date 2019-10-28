079711b2fd77d8d69c5739a070c549389067d023
qiuey00 <qiuey00@gmail.com>
2019-10-28 19:22:27 -0400

    travis testing

--- test_site.py
+++ test_site.py
@@ -13,19 +13,19 @@ def my_app():
 def test_Register(my_app):
     res = my_app.get("/register")
     assert res.status_code == 200
-    assert b"<title>Register</title>" in res.data
+    assert "<title>Register</title>" in res.data
 
 def test_Login(my_app):
     res = my_app.get("/login")
     assert res.status_code == 200
     assert b"<title>Login</title>" in res.data
 
-def test_spell_check(my_app):
+def test_Spell_Check(my_app):
     res = my_app.get("/spell_check")
     assert res.status_code == 200
     assert b"<title>Spell Check</title>" in res.data
 
-def test_register(my_app):
+def test_register_working(my_app):
     res = my_app.get("/register")
     assert res.status_code == 200
     data = BeautifulSoup(res.data, 'html.parser')
