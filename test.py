64e1701fbd637fca4e85353091e1403b7a349e8a
qiuey00 <qiuey00@gmail.com>
2019-10-28 14:37:17 -0400

    travis testing

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ def test_register(my_app):
     assert res.status_code == 200
     assert b"<title>Register</title>" in res.data
 
-def test_register(my_app):
+def test_Login(my_app):
     res = my_app.get("/login")
     assert res.status_code == 200
     assert b"<title>Login</title>" in res.data
 
-def test_register(my_app):
+def test_spell_check(my_app):
     res = my_app.get("/spell_check")
     assert res.status_code == 200
     assert b"<title>Spell Check</title>" in res.data
