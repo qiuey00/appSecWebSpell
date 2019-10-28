106c9d8a334d0757e1a9237e791dde8f70f02274
qiuey00 <qiuey00@gmail.com>
2019-10-28 18:11:13 -0400

    travis testing

--- test_site.py
+++ test_site.py
@@ -10,11 +10,16 @@ def my_app():
     return test_app.test_client()
 
 def test_Login(my_app):
+    res = my_app.get("/register")
+    assert res.status_code == 200
+    assert b"<title>Register</title>" in res.data
+
+def test_Login(my_app):
     res = my_app.get("/login")
     assert res.status_code == 200
-    assert b"<title>Lodfgin</title>" in res.data
+    assert b"<title>Login</title>" in res.data
 
-# def test_spell_check(my_app):
-#     res = my_app.get("/spell_check")
-#     assert res.status_code == 200
-#     assert b"<title>Spell Check</title>" in res.data
+def test_spell_check(my_app):
+    res = my_app.get("/spell_check")
+    assert res.status_code == 200
+    assert b"<title>Spell Check</title>" in res.data
