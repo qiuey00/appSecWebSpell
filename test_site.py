bcaeb22a709fee1222a868511ffd954390760346
qiuey00 <qiuey00@gmail.com>
2019-10-28 16:52:54 -0400

    travis testing

--- test_site.py
+++ test_site.py
@@ -6,17 +6,18 @@ def my_app():
     test_app.debug = True
     return test_app.test_client()
 
-def test_register(my_app):
-    res = my_app.get("/register")
+def test_register():
+	var = my_app()
+    res = var.get("/register")
     assert res.status_code == 200
     assert b"<title>Register</title>" in res.data
 
-def test_Login(my_app):
-    res = my_app.get("/login")
-    assert res.status_code == 200
-    assert b"<title>Login</title>" in res.data
+# def test_Login(my_app):
+#     res = my_app.get("/login")
+#     assert res.status_code == 200
+#     assert b"<title>Login</title>" in res.data
 
-def test_spell_check(my_app):
-    res = my_app.get("/spell_check")
-    assert res.status_code == 200
-    assert b"<title>Spell Check</title>" in res.data
+# def test_spell_check(my_app):
+#     res = my_app.get("/spell_check")
+#     assert res.status_code == 200
+#     assert b"<title>Spell Check</title>" in res.data
