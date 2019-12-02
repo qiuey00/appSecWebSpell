805665a6b421fc2f7ce9b5b7ae44fc9df1e704f2
qiuey00 <qiuey00@gmail.com>
2019-12-02 13:03:40 -0500

    updates

--- app.py
+++ app.py
@@ -306,6 +306,7 @@ def login_history():
         if session.get('logged_in') and request.method == 'POST' and request.form['submit_button'] == 'Check User Login History':
             userToQuery = (form.textbox.data)
             queryResults = userHistory.query.all()
+            print(queryResults)
             return render_template('login_history_results.html', misspelled=queryResults)
         else:
             error='Please Login As Admin'
