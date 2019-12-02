f07f58a155b89c594228a47626082eb6880653c1
qiuey00 <qiuey00@gmail.com>
2019-12-02 14:09:15 -0500

    updates

--- app.py
+++ app.py
@@ -308,7 +308,6 @@ def login_history():
             userToQuery = (form.textbox.data)
             print("did the query fail")
             queryResults = userHistory.query.all()
-            print("no it did not fail")
             username = []
             # action = []
             loginTime = []
@@ -322,6 +321,8 @@ def login_history():
             print(queryResults[0].userAction)
             return render_template('login_history_results.html', login=loginTime, logout=logoutTime)
         else:
+            print("are we not admin")
+
             error='Please Login As Admin'
             return render_template('home.html', form=form, error=error)
     except:
